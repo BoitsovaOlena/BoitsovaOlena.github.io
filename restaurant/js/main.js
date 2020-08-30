89e052a49c9c58fdfddb81321941c14f8792c496
Olena <elena.cerg@gmail.com>
2020-08-30 20:55:48 +0300

    restaurant

---
+++ restaurant/js/main.js
@@ -0,0 +1,689 @@
+"use strict";
+
+var menu = {
+  "soupe": [{
+    "id": "1",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "2",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "3",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "4",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "5",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "6",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "7",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "8",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "9",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "10",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "11",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "12",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "13",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "14",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "15",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "16",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "17",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "18",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "19",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }, {
+    "id": "20",
+    "name": "Name of Soup",
+    "price": "18,11",
+    "category": "soupe",
+    "Ingredients": "List the ingredients in the soup"
+  }],
+  "pizza": [{
+    "id": "1",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "2",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "3",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "4",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "5",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "6",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "7",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "8",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "9",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "10",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "11",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "12",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "13",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "14",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "15",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "16",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "17",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "18",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "19",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "20",
+    "name": "PIZZA FOUR CHEESE",
+    "price": "15,99",
+    "category": "pizza",
+    "Ingredients": "Notes or ingredients"
+  }],
+  "pasta": [{
+    "id": "1",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "2",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "3",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "4",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "5",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "6",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "7",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "8",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "9",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "10",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "11",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "12",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "13",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "14",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "15",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "16",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "17",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "18",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "19",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "20",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }, {
+    "id": "21",
+    "name": "Name of Pasta",
+    "price": "22,10",
+    "category": "pasta",
+    "Ingredients": "List the ingredients in the pasta"
+  }],
+  "salads": [{
+    "id": "1",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }, {
+    "id": "2",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }, {
+    "id": "3",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }, {
+    "id": "4",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }, {
+    "id": "5",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }, {
+    "id": "6",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }, {
+    "id": "7",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }, {
+    "id": "8",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }, {
+    "id": "9",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }, {
+    "id": "10",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }, {
+    "id": "11",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }, {
+    "id": "12",
+    "name": "Name of Salad",
+    "price": "9,99",
+    "category": "salads",
+    "Ingredients": "List the ingredients in the salad"
+  }],
+  "desert": [{
+    "id": "1",
+    "name": "Name of Desert",
+    "price": "12,39",
+    "category": "desert",
+    "Ingredients": "List the ingredients in the desert"
+  }, {
+    "id": "2",
+    "name": "Name of Desert",
+    "price": "12,39",
+    "category": "desert",
+    "Ingredients": "List the ingredients in the desert"
+  }, {
+    "id": "3",
+    "name": "Name of Desert",
+    "price": "12,39",
+    "category": "desert",
+    "Ingredients": "List the ingredients in the desert"
+  }, {
+    "id": "4",
+    "name": "Name of Desert",
+    "price": "12,39",
+    "category": "desert",
+    "Ingredients": "List the ingredients in the desert"
+  }, {
+    "id": "5",
+    "name": "Name of Desert",
+    "price": "12,39",
+    "category": "desert",
+    "Ingredients": "List the ingredients in the desert"
+  }, {
+    "id": "6",
+    "name": "Name of Desert",
+    "price": "12,39",
+    "category": "desert",
+    "Ingredients": "List the ingredients in the desert"
+  }, {
+    "id": "7",
+    "name": "Name of Desert",
+    "price": "12,39",
+    "category": "desert",
+    "Ingredients": "List the ingredients in the desert"
+  }, {
+    "id": "8",
+    "name": "Name of Desert",
+    "price": "12,39",
+    "category": "desert",
+    "Ingredients": "List the ingredients in the desert"
+  }, {
+    "id": "9",
+    "name": "Name of Desert",
+    "price": "12,39",
+    "category": "desert",
+    "Ingredients": "List the ingredients in the desert"
+  }],
+  "drinks": [{
+    "id": "1",
+    "name": "Drink1",
+    "price": "5,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "2",
+    "name": "Drink2",
+    "price": "7,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "3",
+    "name": "Drink3",
+    "price": "5,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "4",
+    "name": "Drink4",
+    "price": "5,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "5",
+    "name": "Drink5",
+    "price": "15,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "6",
+    "name": "Drink6",
+    "price": "5,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "7",
+    "name": "Drink7",
+    "price": "6,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "8",
+    "name": "Drink8",
+    "price": "5,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "9",
+    "name": "Drink9",
+    "price": "12,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "10",
+    "name": "Drink10",
+    "price": "5,00",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "11",
+    "name": "Drink11",
+    "price": "5,39",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "12",
+    "name": "Drink12",
+    "price": "5,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "13",
+    "name": "Drink13",
+    "price": "5,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "14",
+    "name": "Drink14",
+    "price": "5,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "15",
+    "name": "Drink15",
+    "price": "5,15",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "16",
+    "name": "Drink16",
+    "price": "5,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }, {
+    "id": "17",
+    "name": "Drink17",
+    "price": "5,99",
+    "category": "drinks",
+    "Ingredients": "Notes or ingredients"
+  }]
+};
+
+function getCategories(obj) {
+  var list = [];
+
+  for (var key in obj) {
+    list.push(key);
+  }
+
+  return list;
+}
+
+var listCategories = getCategories(menu);
+
+function setCategories(data) {
+  var list = "\n    ".concat(data.map(function (value) {
+    return value === 'pizza' ? " <li class=\"menu__nav-item active\" id=\"".concat(value, "\">").concat(value, "</li>") : " <li class=\"menu__nav-item\" id=\"".concat(value, "\">").concat(value, "</li>");
+  }).join('\n'), "\n  ");
+  document.querySelector('.menu__nav').innerHTML = list;
+}
+
+function getMenuItem(data) {
+  return "\n    <li class=\"menu__list-item\">\n      <div class=\"menu__list-title\">\n        <p>".concat(data.name, "</p>\n        <p>. . . .   ").concat(data.price, " USD</p>\n      </div>\n      <p class=\"menu__list-text\">").concat(data.Ingredients, "</p>\n    </li>\n  ");
+}
+
+function renderItems(data) {
+  var list = "";
+  var contain = document.querySelector('.menu>.container');
+  console.log(contain);
+
+  if (document.body.clientWidth > 624) {
+    contain.classList.contains('small') && contain.classList.remove('small');
+
+    for (var i = 0; i < data.length; i++) {
+      list += getMenuItem(data[i]);
+    }
+  } else {
+    !contain.classList.contains('small') && contain.classList.add('small');
+
+    for (var _i = 0; _i < data.length && _i < 8; _i++) {
+      list += getMenuItem(data[_i]);
+    }
+  }
+
+  document.querySelector('.menu__list').innerHTML = list;
+}
+
+function Selected(evt) {
+  if (!evt.target.classList.contains('active')) {
+    var menus = document.querySelectorAll(".menu__nav-item");
+
+    for (var i = 0; i < menus.length; i++) {
+      if (menus[i].classList.contains('active')) {
+        menus[i].classList.remove('active');
+      }
+    }
+
+    evt.target.classList.add('active');
+    renderItems(menu[evt.target.id]);
+  }
+}
+
+function Resized(evt) {
+  if (evt.target.innerWidth < 690 && evt.target.innerWidth > 600) {
+    var list = document.querySelector(".active").id;
+    renderItems(menu[list]);
+  }
+}
+
+window.onload = function () {
+  setCategories(listCategories);
+  renderItems(menu.pizza);
+  window.addEventListener('resize', Resized, false);
+  var pizza = document.getElementById("pizza");
+  pizza.addEventListener('click', Selected, false);
+  var soupe = document.getElementById("soupe");
+  soupe.addEventListener('click', Selected, false);
+  var pasta = document.getElementById("pasta");
+  pasta.addEventListener('click', Selected, false);
+  var salads = document.getElementById("salads");
+  salads.addEventListener('click', Selected, false);
+  var desert = document.getElementById("desert");
+  desert.addEventListener('click', Selected, false);
+  var drinks = document.getElementById("drinks");
+  drinks.addEventListener('click', Selected, false);
+};
